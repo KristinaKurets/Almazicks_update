2df44037679477e0c75555d28de018fc75f205e5
Кристина Гвоздюкевич <honey_kris@outlook.com>
2020-02-23 13:32:30 +0300

    Add project files.

---
+++ BusinessLogic/Interfaces/IHeroService.cs
@@ -0,0 +1,16 @@
+﻿using Almazicks.DataContracts.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IHeroService
+    {
+        Task<HeroDto> GetHeroAsync(int id);
+        Task<IEnumerable<HeroDto>> GetHeroesAsync();
+        Task CreateHeroAsync(HeroDto hero);
+        Task UpdateHeroAsync(int id, HeroDto hero);
+    }
+}
---
+++ BusinessLogic/Services/HeroService.cs
@@ -0,0 +1,47 @@
+﻿using Almazicks.DataContracts.DataContracts;
+using AutoMapper;
+using BusinessLogic.Interfaces;
+using Data;
+using Data.EntityModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public class HeroService : IHeroService
+    {
+        private readonly DiamondsDbContext _context;
+        private readonly IMapper _mapper;
+
+        public HeroService(DiamondsDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task CreateHeroAsync(HeroDto hero)
+        {
+            await _context.AddAsync(_mapper.Map<Hero>(hero));
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<HeroDto> GetHeroAsync(int id)
+        {
+            var hero = await _context.Heroes.FindAsync(id);
+            return _mapper.Map<HeroDto>(hero);
+        }
+
+        public async Task<IEnumerable<HeroDto>> GetHeroesAsync()
+        {
+            var heroes = await _context.Heroes.ToListAsync();
+            return _mapper.Map<IEnumerable<HeroDto>>(heroes);
+        }
+
+        public Task UpdateHeroAsync(int id, HeroDto hero)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
